bbc6b4d11e24ee1ceb27cc69d6471b73776ec162
Arslan Mahmood <mahmoodarslan584@gmail.com>
2025-04-02 22:20:36 +0500

    Delete server directory

--- server/constants.js
+++
@@ -1,7 +0,0 @@
-export const DB_NAME = "upliftme"
-const options = {
-    httpOnly: true,
-    secure: false, // 🔥 Set to true if using HTTPS
-    sameSite: "lax", // 🔥 Set to 'none' if using HTTPS
-}
-export default  options
